b78a4b54e9a575f350703d4cc91e029ca59668c7
Nguyễn Thiện Tâm <nguyenthientam317@users.noreply.github.com>
2018-12-02 17:01:13 +0700

    update SMTP, Chatbot

--- eCommerce/Controllers/HomeController.cs
+++ eCommerce/Controllers/HomeController.cs
@@ -10,6 +10,30 @@ namespace eCommerce.Controllers
     {
         public ActionResult Index()
         {
+
+            //create a object to hold the message
+            //MailMessage newMessage = new MailMessage();
+
+            //Create addresses
+            //MailAddress senderAddress = new MailAddress("rendoleo317@gmail.com");
+            //MailAddress recipentAddress = new MailAddress("nguyenthientam317@gmail.com");
+
+            //Now create the full message
+            //newMessage.To.Add(recipentAddress);
+            //newMessage.From = senderAddress;
+            //newMessage.Subject = "Tieu de";
+            //newMessage.Body = "Noi dung";
+
+            //Create the SMTP Client object, which do the actual sending
+            //SmtpClient client = new SmtpClient("smtp.gmail.com", 587)
+            //{
+            //    Credentials = new NetworkCredential("rendoleo317@gmail.com", "tfvzcxzscphvqeki"),
+            //    EnableSsl = true
+            //};
+
+            //now send the message
+            //client.Send(newMessage);
+
             return View();
         }
 
