b2984f1cd4cc3c7af4714f032de75edcb7e8ee64
Phu <nguyenngocphu301097@gmail.com>
2018-12-04 00:55:04 +0700

    Version 2.0.2
     Thêm trang quản lí xác nhận tài khoản merchants có controller Index , Detail ( tắt đi ngủ )

--- eCommerce/Areas/Admin/Controllers/MerchantsAccountController.cs
+++ eCommerce/Areas/Admin/Controllers/MerchantsAccountController.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace eCommerce.Areas.Admin.Controllers
 {
     public class MerchantsAccountController : Controller
     {
-        // GET: Admin/MerchantsAccount
-        public ActionResult Index()
+		private MainDbContext db = new MainDbContext();
+		// GET: Admin/MerchantsAccount
+		public ActionResult Index()
         {
-            return View();
+			return View(db.MerchantStores.ToList());
         }
+		public ActionResult Detail()
+		{
+			return View();
+		}
     }
 }
