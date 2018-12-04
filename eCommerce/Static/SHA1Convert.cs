52f360b19d9b41c5712c3e9da8c796bc58a1288a
Nguyễn Thiện Tâm <nguyenthientam317@users.noreply.github.com>
2018-12-04 21:02:30 +0700

    Update v2.1
    - Thêm logo
    - Test SMTP, 123Pay Payment (Comment ở HomeController)
    - Them chatbox
    - Sửa giao diện đăng kí, đăng nhập, quên mật khẩu user

---
+++ eCommerce/Static/SHA1Convert.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace eCommerce.Static
+{
+    public static class SHA1Convert
+    {
+        public static string Hash(string input)
+        {
+            var hash = (new SHA1Managed()).ComputeHash(Encoding.UTF8.GetBytes(input));
+            return string.Join("", hash.Select(b => b.ToString("x2")).ToArray());
+        }
+    }
+}
