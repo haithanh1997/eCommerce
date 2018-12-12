a9694e99c42d3990176729de01954080216419fd
Thanh <trinhhaithanh113@gmail.com>
2018-12-12 16:27:06 +0700

    update v3.1.7

--- eCommerce/Areas/Merchant/Controllers/MerchantHomeController.cs
+++ eCommerce/Areas/Merchant/Controllers/MerchantHomeController.cs
@@ -3,6 +3,7 @@ using eCommerce.EntityFramework;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -67,7 +68,7 @@ namespace eCommerce.Areas.Merchant.Controllers
 		//Hiển thị sản phẩm đăng bán của Merchant
         public ActionResult ManageProduct(string id)
         {
-            return View(db.Products.Where(x => x.Store.User.Id == id));
+            return View(db.Products.Where(x => x.Store.User.Id == id && x.isDisabled == false));
         }
 		//Chi tiết sản phẩm
         public ActionResult Details(long? id)
@@ -109,12 +110,10 @@ namespace eCommerce.Areas.Merchant.Controllers
 		[ValidateAntiForgeryToken]
 		public ActionResult Create(ProductModel model,string id)
         {
-
-
+            ViewBag.UserID = id;
 			Product product = new Product();
             if (ModelState.IsValid)
             {
-				product.Id = model.IdSanpham;
 				product.Store = db.MerchantStores.FirstOrDefault(x=>x.User.Id == id);
 				product.Name = model.Name;
 				product.Price = model.Price;
@@ -136,24 +135,32 @@ namespace eCommerce.Areas.Merchant.Controllers
 				product.AdType = model.AdType;
 				product.isDisabled = model.isDisabled;
 
-		
-                if (model.Image1 != null)
+                bool exists = System.IO.Directory.Exists(Server.MapPath("~/MerchantProduct/"+ViewBag.UserID));
+
+                if (!exists)
+                    System.IO.Directory.CreateDirectory(Server.MapPath("~/MerchantProduct/" + ViewBag.UserID));
+
+
+                var f1 = Request.Files["Image1"];
+                var f2 = Request.Files["Image2"];
+                var f3 = Request.Files["Image3"];
+                if (f1 != null && f1.ContentLength > 0)
                 {
-                    var path = Server.MapPath("~/MerchantProduct/" + model.Image1);
-					
-                    product.Image1 = "/MerchantProduct/" + model.Image1;
+                    var path = Server.MapPath("~/MerchantProduct/" + ViewBag.UserID + "/" + f1.FileName);
+                    f1.SaveAs(path);
+                    product.Image1 = "/MerchantProduct/" + ViewBag.UserID + "/" + f1.FileName;
                 }
-                if (model.Image2 != null  )
+                if (f2 != null && f2.ContentLength > 0)
                 {
-                    var path = Server.MapPath("~/MerchantProduct/" + model.Image2);
-                 
-                    product.Image2 = "/MerchantProduct/" + model.Image2;
+                    var path = Server.MapPath("~/MerchantProduct/" + ViewBag.UserID + "/" + f2.FileName);
+                    f2.SaveAs(path);
+                    product.Image2 = "/MerchantProduct/" + ViewBag.UserID + "/" + f2.FileName;
                 }
-                if (model.Image3 != null )
+                if (f3 != null && f3.ContentLength > 0)
                 {
-                    var path = Server.MapPath("~/MerchantProduct/" + model.Image3);
-                 
-                    product.Image3 = "/MerchantProduct/" + model.Image3;
+                    var path = Server.MapPath("~/MerchantProduct/" + ViewBag.UserID + "/" + f2.FileName);
+                    f3.SaveAs(path);
+                    product.Image3 = "/MerchantProduct/" + ViewBag.UserID + "/" + f2.FileName;
                 }
                 db.Products.Add(product);
                 db.SaveChanges();
@@ -189,7 +196,7 @@ namespace eCommerce.Areas.Merchant.Controllers
             }
 			ProductModel model1 = new ProductModel()
 			{
-				IdSanpham = model.Id,
+				ProductId = model.Id,
 				Store = model.Store,
 				Name = model.Name,
 				Price = model.Price,
@@ -229,13 +236,14 @@ namespace eCommerce.Areas.Merchant.Controllers
         [HttpPost]
 		[ValidateAntiForgeryToken]
 		public ActionResult Edit(ProductModel model)
-			{
-			Product product = db.Products.Find(model.IdSanpham);
+	    {
+			Product product = db.Products.Find(model.ProductId);
+            ViewBag.UserID = db.MerchantStores.FirstOrDefault(x => x.Id == model.Store.Id).User.Id;
 			if (ModelState.IsValid)
             {
 
-				product.Id = model.IdSanpham;
-				product.Store = model.Store;
+				product.Id = model.ProductId;
+				product.Store = db.MerchantStores.FirstOrDefault(x=>x.Id == model.Store.Id);
 				product.Name = model.Name;
 				product.Price = model.Price;
 				product.Quantity = model.Quantity;
@@ -253,32 +261,52 @@ namespace eCommerce.Areas.Merchant.Controllers
 				product.DesignType = model.DesignType;
 				product.Size = model.Size;
 				product.updateDate = DateTime.Now;
-				product.Image1 = model.Image1;
-				product.Image2 = model.Image2;
-				product.Image3 = model.Image3;
-				product.AdType = model.AdType;
+                product.Image1 = model.Image1;
+                product.Image2 = model.Image2;
+                product.Image3 = model.Image3;
+                product.AdType = model.AdType;
 				product.isDisabled = model.isDisabled;
 
-				var f1 = Request.Files["Image1"];
+                bool exists = System.IO.Directory.Exists(Server.MapPath("~/MerchantProduct/" + ViewBag.UserID));
+
+                if (!exists)
+                    System.IO.Directory.CreateDirectory(Server.MapPath("~/MerchantProduct/" + ViewBag.UserID));
+
+                var f1 = Request.Files["Image1"];
                 var f2 = Request.Files["Image2"];
                 var f3 = Request.Files["Image3"];
                 if (f1 != null && f1.ContentLength > 0)
                 {
-                    var path = Server.MapPath("~/MerchantImages/" + f1.FileName);
+                    string fullPath = Request.MapPath(model.Image1);
+                    if (System.IO.File.Exists(fullPath))
+                    {
+                        System.IO.File.Delete(fullPath);
+                    }
+                    var path = Server.MapPath("~/MerchantProduct/" + ViewBag.UserID + "/" + f1.FileName);
                     f1.SaveAs(path);
-					product.Image1 = "/MerchantImages/" + f1.FileName;
+                    product.Image1 = "/MerchantProduct/" + ViewBag.UserID + "/" + f1.FileName;
                 }
                 if (f2 != null && f2.ContentLength > 0)
                 {
-                    var path = Server.MapPath("~/MerchantImages/" + f2.FileName);
+                    string fullPath = Request.MapPath(model.Image2);
+                    if (System.IO.File.Exists(fullPath))
+                    {
+                        System.IO.File.Delete(fullPath);
+                    }
+                    var path = Server.MapPath("~/MerchantProduct/" + ViewBag.UserID + "/" + f2.FileName);
                     f2.SaveAs(path);
-					product.Image2 = "/MerchantImages/" + f2.FileName;
+                    product.Image2 = "/MerchantProduct/" + ViewBag.UserID + "/" + f2.FileName;
                 }
                 if (f3 != null && f3.ContentLength > 0)
                 {
-                    var path = Server.MapPath("~/MerchantImages/" + f3.FileName);
+                    string fullPath = Request.MapPath(model.Image3);
+                    if (System.IO.File.Exists(fullPath))
+                    {
+                        System.IO.File.Delete(fullPath);
+                    }
+                    var path = Server.MapPath("~/MerchantProduct/" + ViewBag.UserID + "/" + f3.FileName);
                     f3.SaveAs(path);
-					product.Image3 = "/MerchantImages/" + f3.FileName;
+                    product.Image3 = "/MerchantProduct/" + ViewBag.UserID + "/" + f3.FileName;
                 }
                 db.Entry(product).State = EntityState.Modified;
                 db.SaveChanges();
@@ -288,7 +316,7 @@ namespace eCommerce.Areas.Merchant.Controllers
 			// Hàng trả về khi thay đổi thất bại :))
 			ProductModel model1 = new ProductModel()
 			{
-				IdSanpham = model.IdSanpham,
+				ProductId = model.ProductId,
 				Store = model.Store,
 				Name = model.Name,
 				Price = model.Price,
@@ -329,7 +357,20 @@ namespace eCommerce.Areas.Merchant.Controllers
         {
             return View(db.Products.Where(x=>x.Price <= 2 && x.Store.User.Id== id));
         }
-		
 
-	}
+        // GET: Admin/Categories/Delete/5
+        public ActionResult Delete(long id)
+        {
+            if(ModelState.IsValid)
+            {
+                var product = db.Products.Find(id);
+                product.isDisabled = true;
+                db.Entry(product).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+    }
 }
