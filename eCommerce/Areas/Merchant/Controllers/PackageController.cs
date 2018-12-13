dea87ecbbebd6f7830edc795aa7769dfca14a0bf
Thành <trinhhaithanh113@gmail.com>
2018-12-13 17:10:25 +0700

    update v3.3.4

--- eCommerce/Areas/Merchant/Controllers/PackageController.cs
+++ eCommerce/Areas/Merchant/Controllers/PackageController.cs
@@ -6,10 +6,21 @@ using System.Web.Mvc;
 
 namespace eCommerce.Areas.Merchant.Controllers
 {
+    [Authorize(Roles = "Merchant")]
     public class PackageController : Controller
     {
+        MainDbContext db = new MainDbContext();
         // GET: Merchant/Package
         public ActionResult Index()
+        {
+            var model = db.Packages.Where(x => x.isDisabled == false).ToList();
+            foreach (var item in model)
+            {
+                item.Price /= 1000;
+            }
+            return View(model);
+        }
+        public ActionResult PurchasePackage(long id)
         {
             return View();
         }
